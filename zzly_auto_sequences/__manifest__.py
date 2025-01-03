80f58aae5dc3ceb06826435de596e39ded601d3d
mapei <mapei@zzly.net>
2025-01-03 11:51:37 +0800

    Merge remote changes

--- zzly_auto_sequences/__manifest__.py
+++ zzly_auto_sequences/__manifest__.py
@@ -18,11 +18,10 @@ Set automatic coding rules for product categories
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Extra Tools',
-<<<<<<< HEAD
+
     'version': '0.3',
-=======
     'version': '0.2',
->>>>>>> origin/master
+
 
     # any module necessary for this one to work correctly
     'depends': ['base','product'],
